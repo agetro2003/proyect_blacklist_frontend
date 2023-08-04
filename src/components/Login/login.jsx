6f18d24d15a401c4d7b03584f3b3e3a9578eaf80
Maulex23 <mauricio23.mcn@gmail.com>
2023-08-03 21:59:00 -0400

    feat: ajustes css login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
                 <div className="row full-height justify-content-center">
                     <div className="col-12 text-center align-self-center py-5">
                         <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                            <h6 className="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+                            {/* <h6 className="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6> */}
                             <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
                             <label htmlFor="reg-log"></label>
                             <div className="card-3d-wrap mx-auto">
@@ -16,7 +16,7 @@ const Login = () => {
                                     <div className="card-front">
                                         <div className="center-wrap">
                                             <div className="section text-center">
-                                                <h4 className="mb-4 pb-3">Log In</h4>
+                                                <h4 className="mb-4 pb-3 login-titulo">Log In</h4>
                                                 <div className="form-group">
                                                     <input type="email" className="form-style" placeholder="Email" />
                                                     <i className="input-icon uil uil-at"></i>
@@ -25,15 +25,17 @@ const Login = () => {
                                                     <input type="password" className="form-style" placeholder="Password" />
                                                     <i className="input-icon uil uil-lock-alt"></i>
                                                 </div>
-                                                <a href="https://www.web-leb.com/code" className="btn mt-4">Login</a>
-                                                <p className="mb-0 mt-4 text-center"><a href="https://www.web-leb.com/code" className="link">Forgot your password?</a></p>
+                                                <div className="login-boton-holder">
+                                                    <a href="#" className="btn mt-4">Login</a>
+                                                </div>
+                                                <p className="mb-0 text-center"><a href="#" className="link">Forgot your password?</a></p>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="card-back">
                                         <div className="center-wrap">
                                             <div className="section text-center">
-                                                <h4 className="mb-3 pb-3">Sign Up</h4>
+                                                <h4 className="mb-3 pb-3 login-titulo">Sign Up</h4>
                                                 <div className="form-group">
                                                     <input type="text" className="form-style" placeholder="Full Name" />
                                                     <i className="input-icon uil uil-user"></i>
@@ -50,7 +52,9 @@ const Login = () => {
                                                     <input type="password" className="form-style" placeholder="Password" />
                                                     <i className="input-icon uil uil-lock-alt"></i>
                                                 </div>
-                                                <a href="https://www.web-leb.com/code" className="btn mt-4">Register</a>
+                                                <div className="login-boton-holder">
+                                                  <a href="#" className="btn mt-4">Register</a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
