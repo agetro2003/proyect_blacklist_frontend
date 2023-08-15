dd10687e6bfe836a1a9e22563c1cfdd414685f66
agetro2003 <jesusdaniolob@gmail.com>
2023-08-15 17:45:08 -0400

    fix deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
-import Login from "./components/Login/Login";
+import Login from "./components/Login/login";
 import Pricing from './components/Pricing/pricing';
-import Main from './components/Main/Main';
-import Test from './components/Test/Test';
 import PageNotFound from './components/PageNotFound/PageNotFound';
 import UnAuthorized from './components/UnAuthorized/UnAuthorized';
 import ProtectedRoute from './components/ProtectedRoute';
