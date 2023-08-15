773e4ca7b32b9d93089869fbabcd48fa1799d0dc
agetro2003 <jesusdaniolob@gmail.com>
2023-08-15 16:49:02 -0400

    add endpoint

--- settings/AxiosInstance.js
+++ settings/AxiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-//https://blacklist-web-service.onrender.com/
 const API_AXIOS = axios.create({
-    baseURL: 'http://127.0.0.1:5000',
+    baseURL: 'https://blacklist-web-service.onrender.com',
     timeout: 10000,
   });
 
