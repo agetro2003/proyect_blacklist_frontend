7de8c158d9a5995a0959058b7c553d9396bac878
agetro2003 <jesusdaniolob@gmail.com>
2023-09-05 18:05:54 -0400

    feat, (SupportForm) Add create_event endpoint to form

--- src/components/Dashboard/SupportForm.jsx
+++ src/components/Dashboard/SupportForm.jsx
@@ -5,19 +5,68 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import Title from './Title';
+import Alert from "../Alert/Alert"
+import Overlay from "../Overlay/Overlay"
+import API_AXIOS from '../../../settings/AxiosInstance';
+import endpointList from '../../../settings/endpoints';
 
 export default function SupportForm() {
   const [motivo, setMotivo] = useState('');
   const [descripcion, setDescripcion] = useState('');
-  const [fechaSoporte, setFechaSoporte] = useState(null);
-
-  const agendarReunion = (event) => {
+  const [fechaSoporte, setFechaSoporte] = useState('');
+  const [loading, setLoading] = useState(false)
+  const [alert, setAlert] = useState({
+  active:false,
+  title: "",
+  content: ""
+}) 
+  const agendarReunion = async(event) => {
     event.preventDefault();
+    let data = {
+      event_title: motivo,
+      event_desc: descripcion,
+      start_date: fechaSoporte,
+      mail_usuario: localStorage.getItem('userEmail').slice(1, localStorage.getItem('userEmail').length -1)
+    }     
+     setLoading(true)
+    try {
+      const res = await API_AXIOS.post(endpointList.create_event, data)
+      console.log(res)
+      setAlert({
+        active:true,
+        title: 'Mensaje',
+        content: res.data.message
+      })
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+          try {
+            setAlert({
+              active: true,
+              title: "Resultado",
+              content:error.response.data.message
+          })
+          } catch (e) {
+            setAlert({
+              active: true,
+              title: "Error",
+              content:e.message
+          })
+          }
+    }
     // Lógica para enviar el formulario
   };
 
   return (
     <form onSubmit={agendarReunion}>
+                  {alert.active && (
+                <Alert open={alert.active} setOpen={setAlert} title={alert.title} content={alert.content}></Alert>
+            )}
+            {loading && (
+             <Overlay>
+             </Overlay>
+             )}
       <Title>Soporte</Title>
       <Grid container spacing={2}>
         <Grid item xs={12}>
@@ -42,7 +91,9 @@ export default function SupportForm() {
         <Grid item xs={12}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DemoContainer components={['DateTimePicker']}>
-                <DateTimePicker label="Fecha de reunión" value={fechaSoporte} />
+                <DateTimePicker label="Fecha de reunión" value={fechaSoporte} onChange={(event) =>{
+                  setFechaSoporte(event.$d)
+                }}/>
                 </DemoContainer>
             </LocalizationProvider>
         </Grid>
